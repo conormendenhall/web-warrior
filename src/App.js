f75cd49174cc35c0be436d940a4a65c791a49d8b
conormendenhall <conor.mendenhall@gmail.com>
2024-02-08 15:20:09 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [warriorName, setWarriorName] = useState("Nameless Warrior");
+  const [status, setStatus] = useState("unsubmitted");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setStatus("submitted");
+  }
+  function handleChange(e) {
+    setWarriorName(e.target.value);
+  }
+
+  const [hero, setHero] = useState({});
+  const [foe, setFoe] = useState({});
+
+  let welcome = "What is your name?";
+  if (status === "submitted") {
+    welcome = `Well met, ${warriorName}.`;
+    hero.name = warriorName;
+    hero.hp = 5;
+    hero.attackDie = 5;
+
+    foe.name = "Goblin";
+    foe.hp = 4;
+    foe.attackDie = 3;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{welcome}</p>
+        {status !== "submitted" && (
+          <form onSubmit={handleSubmit}>
+            <input value={warriorName} onChange={handleChange} />
+            <button disabled={warriorName.length === 0}>Submit</button>
+          </form>
+        )}
+        <div>Hero: {JSON.stringify(hero)}</div>
+        <div>Foe: {JSON.stringify(foe)}</div>
       </header>
     </div>
   );
