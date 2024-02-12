611367cfa3c97bd6b0c79f97e772fedc3b03b8c4
conormendenhall <conor.mendenhall@gmail.com>
2024-02-12 13:05:21 -0500

    move merchantinventory to new file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./App.scss";
+import { MerchantInventory } from "./MerchantInventory";
 
 function App() {
   const freshHero = {
@@ -214,29 +215,3 @@ function App() {
 }
 
 export default App;
-
-const MerchantInventory = ({ heroGold }) => {
-  let merchantInventory = [
-    { name: "Shield", price: 8 },
-    { name: "Leather Armor", price: 10 },
-    { name: "Morning Star", price: 12 },
-    { name: "Chain Mail", price: 14 },
-    { name: "Claymore", price: 16 },
-    { name: "Scale Armor", price: 16 },
-    { name: "Lucerne", price: 18 },
-    { name: "Plate Armor", price: 20 },
-    { name: "Cloak of Invisibility", price: 40 },
-  ];
-  const inventoryItems = merchantInventory.map((item) => (
-    <tr key={item.name} className={heroGold < item.price ? "disabled" : ""}>
-      <td>{item.name}</td>
-      <td>{item.price} GP</td>
-    </tr>
-  ));
-
-  return (
-    <table className="inventory">
-      <tbody>{inventoryItems}</tbody>
-    </table>
-  );
-};
